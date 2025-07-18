3d12c688faa1f3736f0b12a91507f3dda4162c9f
JasonOCat <jason.catbagan@gmail.com>
2025-07-18 14:55:33 +0200

    Add new FontAwesome icons to shared configuration

--- src/app/shared/font-awesome-icons.ts
+++ src/app/shared/font-awesome-icons.ts
@@ -14,4 +14,20 @@ import {faChevronRight} from '@fortawesome/free-solid-svg-icons/faChevronRight';
 
 export const fontAwesomeIcons: IconDefinition[] = [
   faCirclePlay,
+  faHome,
+  faSearch,
+  faBook,
+  faPlus,
+  faChevronLeft,
+  faChevronRight,
+  faUser,
+  faCircleNotch,
+  faCircleXmark,
+  faBackwardStep,
+  faForwardStep,
+  faCirclePause,
+  faCirclePlus,
+  faVolumeMute,
+  faVolumeLow,
+  faVolumeHigh,
 ];
