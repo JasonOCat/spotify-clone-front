c2418b79a84eb0771f678f546bbb7054c3d9529b
JasonOCat <jason.catbagan@gmail.com>
2025-07-18 14:58:01 +0200

    Revamp library component UI and enhance song card display.
    Updated the library layout with a modernized design, including a new header, loading spinner, and song card structure. Integrated reusable `SmallSongCardComponent` and FontAwesome icons for improved functionality and aesthetics. Added SCSS styles for hover effects and responsive design.

---
+++ src/app/home/song-card/song-card.component.ts
@@ -0,0 +1,53 @@
+import {Component, EventEmitter, input, OnInit, output, Output} from '@angular/core';
+import {FontAwesomeModule} from '@fortawesome/angular-fontawesome';
+import { SongDTO } from '../../model/song.model';
+import {animate, style, transition, trigger} from '@angular/animations';
+
+@Component({
+  selector: 'app-song-card',
+  imports: [
+    FontAwesomeModule,
+
+  ],
+  templateUrl: './song-card.component.html',
+  styleUrl: './song-card.component.scss',
+  animations: [
+    trigger(
+      'inOutAnimation',
+      [
+        transition(':enter', [
+          style({transform: 'translateY(10px)', opacity: 0}),
+          animate('.2s ease-out', style({transform: 'translateY(0px)', opacity: 1}))
+        ]),
+        transition(
+          ':leave',
+          [
+            style({transform: 'translateY(0px)', opacity: 1}),
+            animate('.2s ease-in', style({transform: 'translateY(10px)', opacity: 0}))
+          ]
+        ),
+      ])
+  ]
+})
+export class SongCardComponent implements OnInit{
+
+  song = input.required<SongDTO>();
+  songDisplay: SongDTO = {favorite: false, displayPlay: false};
+
+  songToPlay = output<SongDTO>();
+
+  ngOnInit(): void {
+    this.songDisplay = this.song();
+  }
+
+  onHoverPlay(displayIcon: boolean): void {
+    this.songDisplay.displayPlay = displayIcon;
+  }
+
+  play(): void {
+    this.songToPlay.emit(this.songDisplay);
+  }
+
+}
+
+
