dce988725a6153232c684334d12da0d7be37627d
JasonOCat <jason.catbagan@gmail.com>
2025-07-18 13:03:05 +0200

    Add FontAwesome icon support to the application

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,28 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Component, inject, OnInit} from '@angular/core';
+import {RouterOutlet} from '@angular/router';
 import {LibraryComponent} from './layout/library/library.component';
+import {fontAwesomeIcons} from './shared/font-awesome-icons';
+import {FaIconLibrary, FontAwesomeModule} from '@fortawesome/angular-fontawesome';
 
 @Component({
   selector: 'app-root',
-  imports: [LibraryComponent, RouterOutlet],
+  imports: [
+    LibraryComponent,
+    RouterOutlet,
+    FontAwesomeModule
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'spotify-clone-front';
+  private faIconLibrary = inject(FaIconLibrary);
+
+  ngOnInit(): void {
+    this.initFontAwesome();
+  }
+
+  private initFontAwesome() {
+    this.faIconLibrary.addIcons(...fontAwesomeIcons);
+  }
 }
---
+++ src/app/shared/font-awesome-icons.ts
@@ -0,0 +1,17 @@
+import {IconDefinition} from '@fortawesome/free-brands-svg-icons';
+import {
+  faBackwardStep,
+  faBook,
+  faChevronLeft,
+  faCircleNotch, faCirclePause,
+  faCirclePlay, faCirclePlus, faCircleXmark, faForwardStep,
+  faHome,
+  faPlus,
+  faSearch,
+  faUser, faVolumeHigh, faVolumeLow, faVolumeMute
+} from '@fortawesome/free-solid-svg-icons';
+import {faChevronRight} from '@fortawesome/free-solid-svg-icons/faChevronRight';
+
+export const fontAwesomeIcons: IconDefinition[] = [
+  faCirclePlay,
+];
