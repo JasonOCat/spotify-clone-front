d475a5f16252431ec87a53abf5a7a8c8c06db5ac
JasonOCat <jason.catbagan@gmail.com>
2025-07-18 14:57:49 +0200

    Revamp library component UI and enhance song card display.
    Updated the library layout with a modernized design, including a new header, loading spinner, and song card structure. Integrated reusable `SmallSongCardComponent` and FontAwesome icons for improved functionality and aesthetics. Added SCSS styles for hover effects and responsive design.

---
+++ src/app/shared/small-song-card/small-song-card.component.ts
@@ -0,0 +1,22 @@
+import {Component, input, output} from '@angular/core';
+import { SongDTO } from '../../model/song.model';
+;
+
+@Component({
+  selector: 'app-small-song-card',
+  imports: [],
+  templateUrl: './small-song-card.component.html',
+  styleUrl: './small-song-card.component.scss'
+})
+export class SmallSongCardComponent {
+
+  song = input.required<SongDTO>();
+
+  songToPlay = output<SongDTO>();
+
+  play() {
+    this.songToPlay.emit(this.song());
+  }
+
+
+}
